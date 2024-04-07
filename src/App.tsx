32ee4384295413dab3754e9791c343aa6aae1666
Bivek Gurung <bivek.tamu@gmail.com>
2024-04-07 10:04:02 +1000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-// import { ChangeEvent, useState } from 'react'
 import './App.css'
-import Dots from './components/Dots'
-// import Counter from './components/Counter'
-// import Input from './components/Input'
+import Toast from './components/Toast'
 
 function App() {
-  // const [count, setCount] = useState<number>(0)
-  // const [text, setText] = useState<string>('')
- 
-  // const updateText = (e:ChangeEvent<HTMLInputElement>) => {
-  //   setText(e.target.value)
-  // }
+
 
   return (
     <>
-     {/* <Counter count={count} setCount={setCount} /> */}
-     {/* <Input text={text} updateText={(e:ChangeEvent<HTMLInputElement>)=>updateText(e)} /> */}
-     <Dots />
+      <Toast />
     </>
   )
 }
---
+++ src/components/Toast.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Toast {
+    
+}
+const Toast = () => {
+
+  return (
+    <div>Toast</div>
+  )
+}
+
+export default Toast
